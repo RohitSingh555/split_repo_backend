1a11050ab4b5e2b70b0009f1771bdc7f67e1fba6
umangd98 <umangd98@gmail.com>
2024-05-02 18:22:45 +0530

    added cors for vercel

--- split-stay-backend/app.js
+++ split-stay-backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://planner-portal.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })

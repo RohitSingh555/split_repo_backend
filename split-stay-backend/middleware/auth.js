bfc8c674942c2d3a783d64ee692376577e643286
RohitSingh555 <singhrg@rknec.edu>
2024-05-02 16:52:46 +0530

    Fixed oAuth Authorization issues

--- split-stay-backend/middleware/auth.js
+++ split-stay-backend/middleware/auth.js
@@ -1,40 +1,40 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../model/userModel/userModel.js";
+import AuthUser from "../model/userModel/authModel.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
-  // get token //
-  let token;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    // decode the token //
     try {
-      token = req.headers.authorization.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       const id = decoded.userId;
-      req.id = id;
-      const user = await User.findById(id).select("-password");
-      req.user = user;
 
-      next();
-    } catch (err) {
-      res.status(401).json({
-        success: false,
-        message: "Not Authorized, Token Failed",
-      });
-      throw new Error("Not Authorized, Token Failed");
+      const user = await User.findById(id).select("-password");
+      if (user) {
+        req.user = user;
+        return next();
+      }
+    } catch (err) {}
+  }
+
+  if (req.headers.authorization && req.headers.email) {
+    const token = req.headers.authorization.split(" ")[1];
+    const email = req.headers.email;
+    const authUser = await AuthUser.findOne({ email });
+    if (authUser) {
+      req.user = authUser;
+      return next();
     }
   }
 
-  if (!token) {
-    res.status(401).json({
-      success: false,
-      message: "Not Authorized To Access This Route",
-    });
-    throw new Error("Not Authorized To Access This Route");
-  }
+  res.status(401).json({
+    success: false,
+    message: "Not Authorized, Token Missing or Invalid",
+  });
 });
 
 export const authRole = (role) => {
